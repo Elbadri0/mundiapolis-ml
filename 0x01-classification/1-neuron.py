be3e7796e6f77604f104516a195031ce2d2a52b1
Abdelhamid elbadri <39597183+Elbadri0@users.noreply.github.com>
2021-03-21 23:42:47 +0100

    Update 1-neuron.py

--- 0x01-classification/1-neuron.py
+++ 0x01-classification/1-neuron.py
@@ -1,46 +1,46 @@
 #!/usr/bin/env python3
-'''the neruon class
+'''Module containing the neruon class
 '''
 
 import numpy as np
 
 
 class Neuron:
-    '''Class that initialized a neuron
+    '''Class that defines a neuron
     '''
 
     def __init__(self, nx):
-        '''the function for the Neuron class
-        
-        input.
-            nx: The number of input of the neuron.
+        '''Initialization function for the Neuron class
+
+        Args.
+            nx: The number of input features to the neuron.
         '''
 
         if not isinstance(nx, int):
             raise TypeError('nx must be an integer')
         if nx < 1:
             raise ValueError('nx must be a positive integer')
-        self.W = np.random.normal(0, 1, (1, nx))
-        self.b = 0
-        self.A = 0
+        self.__W = np.random.normal(0, 1, (1, nx))
+        self.__b = 0
+        self.__A = 0
 
     @property
     def W(self):
-        '''Returns __W
+        '''Returns the value of __W
         '''
 
         return self.__W
 
     @property
     def b(self):
-        '''Returns __b
+        '''Returns the value of __b
         '''
 
         return self.__b
 
     @property
     def A(self):
-        '''Returns __A
+        '''Returns the value of __A
         '''
 
         return self.__A
