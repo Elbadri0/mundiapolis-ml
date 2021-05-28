2f5a34477434fc848bdce4e9a560dcca26d57081
Abdelhamid elbadri <39597183+Elbadri0@users.noreply.github.com>
2021-05-28 14:32:29 +0100

    Update 4-calculate_loss.py

--- 0x02-tensorflow/4-calculate_loss.py
+++ 0x02-tensorflow/4-calculate_loss.py
@@ -7,6 +7,4 @@ def calculate_loss(y, y_pred):
     '''Function that calculates
     the softmax cross-entropy
     loss of a prediction'''
-    return tf.losses.softmax_cross_entropy(
-        y, y_pred
-    )
+    return tf.losses.softmax_cross_entropy(y, y_pred)
